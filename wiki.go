e429c0a693ef63267ccd4106213688ceb6c87496
Josh Roppo <joshroppo@gmail.com>
2014-06-29 11:47:31 -0700

    Fixed form submission to update the pages
    Savehandler added to create and save pages edited via the edit page
    form.

--- wiki.go
+++ wiki.go
@@ -30,13 +30,19 @@ func base_path(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, I love %s!", r.URL.Path[1:])
 }
 
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
+}
+
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	p, err := loadPage(title)
 	if err != nil {
-		fmt.Fprintf(w, "There was an error")
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
 	}
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+	renderTemplate(w, "view", p)
 }
 
 func editFile(w http.ResponseWriter, r *http.Request) {
@@ -45,13 +51,21 @@ func editFile(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		p = &Page{Title: title}
 	}
-	t, _ := template.ParseFiles("edit.html")
-	t.Execute(w, p)
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
 func main() {
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editFile)
+	http.HandleFunc("/save/", saveHandler)
 	http.HandleFunc("/", base_path)
 	http.ListenAndServe(":8080", nil)
 }
