ecd26e6b98733ad301cbc3d1e933aa1450c1e49d
Josh Roppo <joshroppo@gmail.com>
2014-08-13 14:31:27 -0700

    Basic vertical bar charts working

--- static/js/svgbars.js
+++ static/js/svgbars.js
@@ -1,31 +1,34 @@
 
-var width = 500;
+var width = 960;
+var height = 500;
+
+var y = d3.scale.linear().range([height, 0]);
+
 var barHeight = 20;
 
-var x = d3.scale.linear()
-    .range([0, width]);
-
 var chart = d3.select(".chart")
-    .attr("width", width);
+    .attr("width", width)
+    .attr("height", height);
 
 d3.tsv("static/data/charts.tsv", type, function(error, data) {
-    x.domain([0, d3.max(data, function(d){return d.value;})]);
+    y.domain([0, d3.max(data, function(d){return d.value;})]);
 
-    chart.attr("height", barHeight * data.length);
+    var barWidth = width / data.length
 
     var bar = chart.selectAll("g")
         .data(data)
         .enter().append("g")
-        .attr("transform", function(d, i){ return "translate(0," + i * barHeight + ")"; });
+        .attr("transform", function(d, i){ return "translate(" + i * barWidth + ",0)"; });
 
     bar.append("rect")
-        .attr("width", function(d){ return x(d.value); })
-        .attr("height", barHeight -1);
+        .attr("y", function(d) { return y(d.value); })
+        .attr("height", function(d){ return height - y(d.value); })
+        .attr("width", barWidth -1);
 
     bar.append("text")
-        .attr("x", function(d) { return x(d.value) - 3; })
-        .attr("y", barHeight/2)
-        .attr("dy", ".35em")
+        .attr("y", function(d) { return x(d.value) + 3; })
+        .attr("x", barWidth/2)
+        .attr("dy", ".75em")
         .text(function(d) { return d.value; });
 });
 
