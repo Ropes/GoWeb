96551837c86a0b3de64022dffa31b2115e56e554
Josh Roppo <joshroppo@gmail.com>
2014-08-13 16:03:43 -0700

    Displaying the bar chart with scales and names

--- static/js/svgbars.js
+++ static/js/svgbars.js
@@ -2,9 +2,17 @@ var margin = {top: 20, right:30, left: 30, bottom: 40},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
+var x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
 var y = d3.scale.linear().range([height, 0]);
 
 var barHeight = 20;
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
 
 var chart = d3.select(".chart")
     .attr("width", width + margin.left + margin.right)
@@ -13,25 +21,27 @@ var chart = d3.select(".chart")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 d3.tsv("static/data/charts.tsv", type, function(error, data) {
+    x.domain(data.map(function(d){ return d.name; }));
     y.domain([0, d3.max(data, function(d){return d.value;})]);
 
-    var barWidth = width / data.length
+    chart.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0, " + height + ")")
+        .call(xAxis);
 
-    var bar = chart.selectAll("g")
+    chart.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+
+    chart.selectAll(".bar")
         .data(data)
-        .enter().append("g")
-        .attr("transform", function(d, i){ return "translate(" + i * barWidth + ",0)"; });
+        .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d){ return x(d.name); })
+            .attr("y", function(d){ return y(d.value); })
+            .attr("height", function(d) { return height - y(d.value); })
+            .attr("width", x.rangeBand());
 
-    bar.append("rect")
-        .attr("y", function(d) { return y(d.value); })
-        .attr("height", function(d){ return height - y(d.value); })
-        .attr("width", barWidth -1);
-
-    bar.append("text")
-        .attr("y", function(d) { return y(d.value) + 3; })
-        .attr("x", barWidth/2)
-        .attr("dy", ".75em")
-        .text(function(d) { return d.value; });
 });
 
 function type(d) {
